00ff39284a1cf7f308b7ea8b8ebd2c87b11a1dc5
Ansh Katiyar <katiyaransh952@gmail.com>
2025-03-16 14:00:15 +0530

    updating and adding

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,9 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PageController{
@@ -12,5 +15,17 @@ public class PageController{
         model.addAttribute("youtube","Akriti code");
         model.addAttribute("github_repo","https://github.com/royalkatiyar40/");
         return "home";
+    }
+
+    //about route
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return"about";
+    }
+    
+    //services
+    @RequestMapping("/services")
+    public String servicesPage(){
+        return"services";
     }
 }
