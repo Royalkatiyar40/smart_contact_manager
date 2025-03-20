93eb35875354bd3a9db8f83fcf369b951415b2ed
Ansh Katiyar <katiyaransh952@gmail.com>
2025-03-20 15:46:23 +0530

    video14

--- src/main/java/com/scm/forms/UserForm.java
+++ src/main/java/com/scm/forms/UserForm.java
@@ -1,5 +1,8 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +17,20 @@ import lombok.ToString;
 @Builder
 @ToString
 public class UserForm {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 4,message = "Min 4 Characters is required")
     private String name;
+    
+    @Email(message = "Invalid Email Address")
+    @NotBlank(message = "Email is required")
     private String email;
+    @NotBlank(message = "Password is Required")
+    @Size(min = 8,message = "Min 8 Characters is required")
     private String password;
+    @Size(message = "About is required")
     private String about;
+    @Size(min = 10, max=10 ,message = "Invalid Phone Number")
     private String phoneNumber;
 
 }
